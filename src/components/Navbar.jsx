49ba008f7dc5a1072ffe78d7533c707de00c43af
fatihadriansyah <muhammadfatih1629@gmail.com>
2026-06-23 05:42:46 +0700

    Pesan pembaruan Anda

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="fixed w-full z-50 bg-white/60 backdrop-blur-md shadow-sm border-b border-white/50">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold text-red-700 tracking-widest">
-          KYOTO<span className="text-gray-800">.</span>
+          EXPLORE KYOTO<span className="text-gray-800">.</span>
         </Link>
         <div className="hidden md:flex gap-8 font-semibold text-gray-700">
           <Link to="/" className="hover:text-red-600 transition duration-300">Beranda</Link>
